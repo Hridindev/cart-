ccc55a13ee7aaec8882afcfcd2cad6815fbf7e6c
Hridindev <hridindevdev@gmail.com>
2026-06-19 11:24:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// Sidebar navigation toggle functions
+function openSidebar() {
+    document.getElementById("side-navbar").classList.add("active-drawer");
+}
+
+function closeSideBar() {
+    document.getElementById("side-navbar").classList.remove("active-drawer");
+}
